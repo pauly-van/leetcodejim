abe40f4e2024f5352b5dfbd690146cb72a1011bd
pauly <paulyv8n@gmail.com>
2022-03-28 12:15:24 +0000

    initial commit

---
+++ twoPlus.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+
+"""Input: nums = [2,7,11,15], target = 9 # 
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]."""
+
+
+def twoPlusIndex(arrayOfNums, target):
+  for x in range(len(arrayOfNums)):
+    for n in range(len(arrayOfNums)-1):
+      if arrayOfNums[x] + arrayOfNums[n+1] == target:
+        return x,n+1
+
+def assertValue(actual, expected, description):
+   if(actual == expected):
+     print('The test passed')
+   else:
+     print('Failed: We expected ', expected, 'but got ', actual, description)
+
+
+assertValue(twoPlusIndex([2,7,11,15], 9), (0,1), "Should find two numbers in a list of numbers to equal target")
+assertValue(twoPlusIndex([3,2,4], 6), (1,2), "Should find two numbers in a list of numbers to equal target")
+assertValue(twoPlusIndex([3,3], 6), (0,1), "Should find two numbers in a list of numbers to equal target")
+
+
+   
