ecaee21552eb5f03f4df04162150d5b932a7d3bd
pauly <paulyv8n@gmail.com>
2022-03-30 13:50:10 +0000

    allnmjost branch

--- RomanToInt.py
+++ RomanToInt.py
@@ -19,9 +19,12 @@ Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
 
+from ast import Num, Str
+
+
 def romanToInt(s):
   #create a table to define each roman number to a value 
-  table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+  table = {"I": 1, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
   # Break input into list [X, I, V]
   input = list(s) #[X, I, V]
   #loop thru table to find value
@@ -29,8 +32,19 @@ def romanToInt(s):
   keys = list(table.keys()) #an array of keys ['I', 'V', 'X', 'L', 'C', 'D', 'M']
   for r in range(len(input)): 
     # another loop that goes thru keys
+    print(total)
     for n in range(len(keys)):
-      if input[r] == keys[n]:
+      try:
+        outOfRange = input[r+1]
+      except:
+        break
+      key = input[r]+input[r+1] 
+      if key in table.keys():
+        total = total + table[key]
+        r += 2
+        if r>=len(input):
+          break
+      elif input[r] == keys[n]:
     # If is equal to one of the keys
         total = total + table[input[r]] 
         continue
@@ -40,7 +54,7 @@ def assertRoman(actual, expected, description):
   if actual == expected:
     print('Test passed')
   else:
-    print('Expected '+str(expected)+'but got '+str(actual)+ ': '+description)
+    print('Expected '+str(expected)+' but got '+str(actual)+ ': '+description)
 
 assertRoman(romanToInt("CMXLIX"), 949, "Supposed to return int from roman")
 assertRoman(romanToInt("LIX"), 59, "Supposed to return int from roman")
